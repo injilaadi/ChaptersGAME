c6986da02d2d151fdc6ecee0fd7c4fb51ca379bd
injilaadi <injilaadil@gmail.com>
2024-05-27 17:39:14 -0700

    game

---
+++ src/Game.java
@@ -0,0 +1,22 @@
+public class Game{
+    ChoiceHandler handle = new ChoiceHandler(this);
+    State ui = new State();
+    GameState vm = new GameState(ui);
+    Story story = new Story(this, ui, vm);
+
+    String nextPosition1, nextPosition2, nextPosition3;
+
+    public Game(){
+        ui.createUI(handle);
+        story.defaultSetup();
+        vm.showTitleScreen();
+
+    }
+
+    public static void main(String[] args){
+        Game launch = new Game();
+
+
+    }
+
+}
